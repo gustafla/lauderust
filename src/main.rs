1ab20246792701b5d3f980ed6fd99073df2ebade
Lauri Gustafsson <me@gustafla.space>
2022-05-05 10:38:57 +0300

    Add anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct User {
+    id: String,
+    name: String,
+    email: String,
+    from_flight_id: String,
+    to_flight_id: String,
+    image_url: String,
+    activity_id: String,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Flight {
+    id: String,
+    name: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    Ok(())
 }
