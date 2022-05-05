227a1f53602098cfe383c183bc911394e2134767
Lauri Gustafsson <me@gustafla.space>
2022-05-05 13:49:42 +0300

    Add Yew example application

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use anyhow::{Context, Result};
 use serde::Deserialize;
+use yew::prelude::*;
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -34,16 +34,45 @@ struct UserLocation {
     coordinates: Location,
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    println!(
-        "{:#?}",
-        reqwest::get("https://hackathlon.nitorio.us/coordinates")
-            .await
-            .context("Cannot fetch user information")?
-            .json::<Vec<UserLocation>>()
-            .await?
-    );
+enum Msg {
+    AddOne,
+}
 
-    Ok(())
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
 }
